5fc8a72391ea04bc88be9a0348b74de5ba25b124
xjasonlyu <xjasonlyu@gmail.com>
2024-12-22 14:42:22 -0500

    Add SyntaxException

--- jpipe_runner/exceptions.py
+++ jpipe_runner/exceptions.py
@@ -8,3 +8,7 @@ This module contains the set of jPipe Runner's exceptions.
 
 class RunnerException(Exception):
     """There was an ambiguous exception that occurred while running the runner."""
+
+
+class SyntaxException(RunnerException):
+    """A syntax error occurred."""
