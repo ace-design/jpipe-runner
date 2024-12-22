d941db26d58abdfc295ff476f1b8593e5cac50a0
xjasonlyu <xjasonlyu@gmail.com>
2024-12-22 12:50:07 -0500

    update main entries

---
+++ jpipe_runner/exceptions.py
@@ -0,0 +1,10 @@
+"""
+jpipe_runner.exceptions
+~~~~~~~~~~~~~~~~~~~~~~~
+
+This module contains the set of jPipe Runner's exceptions.
+"""
+
+
+class RunnerException(Exception):
+    """There was an ambiguous exception that occurred while running the runner."""
---
+++ jpipe_runner/runner.py
@@ -0,0 +1,14 @@
+"""
+jpipe_runner.runner
+~~~~~~~~~~~~~~~~~~~
+
+This module contains the entrypoint of jPipe Runner.
+"""
+
+
+def main():
+    print('runner')
+
+
+if __name__ == '__main__':
+    main()
