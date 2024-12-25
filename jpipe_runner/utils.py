961f4bed0733dd4cc085c9c081c421fe71921807
xjasonlyu <xjasonlyu@gmail.com>
2024-12-24 19:46:53 -0500

    add sanitize_string helper func

--- jpipe_runner/utils.py
+++ jpipe_runner/utils.py
@@ -6,6 +6,7 @@ This module contains the utilities of jPipe Runner.
 """
 
 import json
+import re
 
 
 def unquote_string(s: str) -> str:
@@ -15,16 +16,25 @@ def unquote_string(s: str) -> str:
         raise ValueError(f'{repr(s)} is not a valid STRING') from e
 
 
-def _test_unquote_string():
+def sanitize_string(s: str) -> str:
+    # Use re to keep only allowed characters.
+    sanitized = re.sub(r'[^a-z0-9_]', '',
+                       re.sub(r'\s+', '_', s.lower()))
+    return sanitized
+
+
+def _test():
+    """test unquote_string"""
     assert unquote_string('"hello"') == 'hello'
     try:
         unquote_string("'hello'")
     except ValueError:
         pass
 
-
-def _test():
-    _test_unquote_string()
+    """test sanitize_string"""
+    assert sanitize_string('Hello, world!') == 'hello_world'
+    assert sanitize_string('Check  contents w.r.t. NDA') == 'check_contents_wrt_nda'
+    assert sanitize_string('Check PEP8 coding standard') == 'check_pep8_coding_standard'
 
 
 if __name__ == "__main__":
