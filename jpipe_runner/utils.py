27d2cda5507a2fe70c39c4c73f4606d9139d9e3e
xjasonlyu <xjasonlyu@gmail.com>
2024-12-24 21:56:57 -0500

    fix utils.sanitize_string

--- jpipe_runner/utils.py
+++ jpipe_runner/utils.py
@@ -19,7 +19,8 @@ def unquote_string(s: str) -> str:
 def sanitize_string(s: str) -> str:
     # Use re to keep only allowed characters.
     sanitized = re.sub(r'[^a-z0-9_]', '',
-                       re.sub(r'\s+', '_', s.lower()))
+                       re.sub(r'\s+', '_',
+                              s.lower().strip()))
     return sanitized
 
 
@@ -32,8 +33,8 @@ def _test():
         pass
 
     """test sanitize_string"""
-    assert sanitize_string('Hello, world!') == 'hello_world'
-    assert sanitize_string('Check  contents w.r.t. NDA') == 'check_contents_wrt_nda'
+    assert sanitize_string('Hello,              world!') == 'hello_world'
+    assert sanitize_string('Check contents w.r.t. NDA ') == 'check_contents_wrt_nda'
     assert sanitize_string('Check PEP8 coding standard') == 'check_pep8_coding_standard'
 
 
