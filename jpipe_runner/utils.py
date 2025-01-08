3a6181af6fa3330d953c49b55e4bc20d94a2cd2c
xjasonlyu <xjasonlyu@gmail.com>
2025-01-08 16:25:30 -0500

    feat: improve snake case conversion

--- jpipe_runner/utils.py
+++ jpipe_runner/utils.py
@@ -17,11 +17,13 @@ def unquote_string(s: str) -> str:
 
 
 def sanitize_string(s: str) -> str:
+    # Convert to snake case
+    # Ref: https://stackoverflow.com/a/1176023/9243111
+    s = re.sub(r'(?<=[a-z])(?=[A-Z])|(?<=[A-Z])(?=[A-Z][a-z])', '_', s).lower()
     # Use re to keep only allowed characters.
     sanitized = re.sub(r'[^a-z0-9_]', '',
                        re.sub(r'\s+', '_',
-                              re.sub(r'[/|\\]', ' ', s)
-                              .lower().strip()))
+                              re.sub(r'[/|\\]', ' ', s).strip()))
     return sanitized
 
 
@@ -38,6 +40,8 @@ def _test():
     assert sanitize_string('Check contents w.r.t. NDA ') == 'check_contents_wrt_nda'
     assert sanitize_string('Check PEP8 coding standard') == 'check_pep8_coding_standard'
     assert sanitize_string('Check        Grammar/Typos') == 'check_grammar_typos'
+    assert sanitize_string('Check is valid HTTPHeader ') == 'check_is_valid_http_header'
+    assert sanitize_string('Check enabled PodDisruptionBudget') == 'check_enabled_pod_disruption_budget'
 
 
 if __name__ == "__main__":
